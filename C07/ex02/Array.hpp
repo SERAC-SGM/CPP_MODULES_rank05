813b9e933d791d0941c81728a074b74002f8e2e6
lletourn <lletourn@student.42.fr>
2023-09-13 17:39:38 +0200

    c07 segfault

--- C07/ex02/Array.hpp
+++ C07/ex02/Array.hpp
@@ -1,38 +1,61 @@
 #ifndef ARRAY_H
 # define ARRAY_H
 
+#include <stdexcept>
+
 template<typename T>
 class Array
 {
 public:
 
-	Array()
+	Array(): _array(0), _size(0) {}
+
+	Array(unsigned int n): _size(n)
 	{
-		this->_array = new T[0];
+		this->_array = new T[n];
+		for (unsigned i = 0; i < n; i++)
+			this->_array[i] = T();
 	}
 
-	Array(unsigned int n)
-	{
-		this->array = new T[n];
-	}
-
-	Array(const Array &toCopy)
+	Array(const Array &toCopy): _size(toCopy._size)
 	{
 		*this = toCopy;
 	}
 
 	~Array()
 	{
+		//std::cout << "destructor called" << std::endl;
 		delete this->_array;
 	}
 
-	unsigned int	size() const;
+	unsigned int	size() const
+	{
+		return this->_size;
+	}
 
-	Array	&operator=(const Array &rhs);
+	Array	&operator=(const Array &rhs)
+	{
+		if (this == &rhs)
+			return *this;
+		//delete [] this->_array;
+		this->_size = rhs._size;
+		this->_array = new T[this->_size];
+		for (unsigned int i = 0; i < this->_size; i++)
+			this->_array[i] = rhs._array[i];
+		return *this;
+	}
+
+	T	&operator[](unsigned int index)
+	{
+		if (index >= this->_size)
+			throw std::out_of_range("Index out of range");
+		return this->_array[index];
+	}
 
 protected:
 
-	T *_array;
+	T				*_array;
+	unsigned int	_size;
 
 };
 #endif
